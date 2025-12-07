c6a154fb18feca96331c78a48ac8dd5a6883b175
mohamed hassan <63217614+Moamed1010@users.noreply.github.com>
2025-12-07 02:59:01 +0200

    add custom list view item & featured list view

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+      padding: const EdgeInsets.only(right: 24,left: 24, top: 20,bottom: 25),
       child: Row(
         children: [
           Image.asset(AssetsData.logo, height: 25),
---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,17 @@
+import 'package:book/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: ClipRRect(
+        borderRadius: BorderRadiusGeometry.circular(16),
+        child: Image.asset(AssetsData.testImagese, fit: BoxFit.fill),
+      ),
+    );
+  }
+}
